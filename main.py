4877eedd88a30a080434d9750ead233d167be10f
Liza <you@example.com>
2022-07-20 09:05:40 +0300

    finish

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from threading import Thread
 import random
 import pygame as pg
 
+
 pg.init()
 
 win = pg.display.set_mode((541, 541))
 win.fill((138, 43, 226))
-head = pg.image.load('snake_head.png')
 body = pg.image.load('snake_body.png')
+head = pg.image.load('snake_head.png')
 black_square = pg.image.load('bl_sq.png')
+gameover = pg.image.load('gameover.png')
+gameover = pg.transform.scale(gameover, (401, 171))
 food = pg.image.load('food.png')
 board = [['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
          ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
@@ -35,111 +38,134 @@ board = [['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', 
          ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
          ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']]
 run = True
-snake = [[2, 0],
+game_over = False
+snake = [[0, 0],
          [1, 0],
-         [0, 0]]
+         [2, 0]
+         ]
+current_direction = 'right'
 
 
-def print_food():
+def place_food():
     global run
+    count = 0
 
     while run:
-        x = random.randrange(0, 20)
-        y = random.randrange(0, 20)
-        board[y][x] = 3
-        time.sleep(3)
+        for i in range(1, 19):
+            for j in range(1, 19):
+                if board[i][j] == 3:
+                    count = count + 1
+        if count == 0:
+            x = random.randrange(1, 19)
+            y = random.randrange(1, 19)
+            board[y][x] = 3
+        count = 0
+        time.sleep(0.003)
 
 
-
-
-
-def print_snake():
+def place_snake():
     global run
+    global board
 
-    while run:
-        for cell in snake:
-            x = cell[0]
-            y = cell[1]
-            board[y][x] = 1
+    for cell in snake:
+        snake_x = cell[0]
+        snake_y = cell[1]
+        board[snake_y][snake_x] = 1
 
-
-
-
-
+    head_x = snake[-1][0]
+    head_y = snake[-1][1]
+    board[head_y][head_x] = 2
 
 
 def click_buttons():
+    global run
+    global game_over
+    global current_direction
     global event
     global board
     global snake
+    if game_over is True:
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if 374 <= event.pos[0] <= 417 and 281 <= event.pos[1] <= 305:
+                    pg.quit()
+                    exit(0)
+                if 98 <= event.pos[0] <= 188 and 281 <= event.pos[1] <= 305:
+                    game_over = False
+                    win.fill((138, 43, 226))
+                    board = [
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_'],
+                        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_',
+                         '_']]
+                    snake = [[0, 0],
+                             [1, 0],
+                             [2, 0]
+                             ]
 
-    if event.key == pg.K_RIGHT and snake[0][0] == 19:
-        x = snake[0][0]
-        y = snake[0][1]
-        board[y][x] = '_'
-        snake = snake[1:]
-        snake[-1][0] = 0
-        if board[snake[-1][1]][snake[-1][0]+1] != 3:
-            snake.append([0, snake[-1][1]])
-
-        elif board[snake[-1][1]][snake[-1][0]+1] == 3:
-            snake.append([0, snake[-1][1]])
-            snake.append([1, snake[-1][1]])
-        board[y][x] = 0
-
-    if event.key == pg.K_RIGHT and board[snake[-1][1]][snake[-1][0]+1] != 1:
-        x = snake[0][0]
-        y = snake[0][1]
-        board[y][x] = '_'
-        snake = snake[1:]
-        if board[snake[-1][1]][snake[-1][0]+1] != 3:
-            snake.append([snake[-1][0]+1, snake[-1][1]])
-        elif board[snake[-1][1]][snake[-1][0]+1] == 3:
-            snake.append([snake[-1][0]+1, snake[-1][1]])
-            snake.append([snake[-1][0]+1, snake[-1][1]])
-
-    elif event.key == pg.K_LEFT and board[snake[-1][1]][snake[-1][0]-1] != 1:
-        x = snake[0][0]
-        y = snake[0][1]
-        board[y][x] = '_'
-        snake = snake[1:]
-        if board[snake[-1][1]][snake[-1][0]-1] != 3:
-            snake.append([snake[-1][0]-1, snake[-1][1]])
-        elif board[snake[-1][1]][snake[-1][0]-1] == 3:
-            snake.append([snake[-1][0]-1, snake[-1][1]])
-            snake.append([snake[-1][0]-1, snake[-1][1]])
-
-    elif event.key == pg.K_UP and board[snake[-1][1]-1][snake[-1][0]] != 1:
-        x = snake[0][0]
-        y = snake[0][1]
-        board[y][x] = '_'
-        snake = snake[1:]
-        if board[snake[-1][1]-1][snake[-1][0]] != 3:
-            snake.append([snake[-1][0], snake[-1][1]-1])
-        elif board[snake[-1][1]-1][snake[-1][0]] == 3:
-            snake.append([snake[-1][0], snake[-1][1]-1])
-            snake.append([snake[-1][0], snake[-1][1]-1])
-
-    elif event.key == pg.K_DOWN and board[snake[-1][1]+1][snake[-1][0]] != 1:
-        x = snake[0][0]
-        y = snake[0][1]
-        board[y][x] = '_'
-        snake = snake[1:]
-        if board[snake[-1][1]+1][snake[-1][0]] != 3:
-            snake.append([snake[-1][0], snake[-1][1]+1])
-        elif board[snake[-1][1]+1][snake[-1][0]] == 3:
-            snake.append([snake[-1][0], snake[-1][1]+1])
-            snake.append([snake[-1][0], snake[-1][1]+1])
-
-
+    if event.type == pg.KEYDOWN:
+        if event.key == pg.K_RIGHT and current_direction != 'left':
+            current_direction = 'right'
+        elif event.key == pg.K_LEFT and current_direction != 'right':
+            current_direction = 'left'
+        elif event.key == pg.K_UP and current_direction != 'down':
+            current_direction = 'up'
+        elif event.key == pg.K_DOWN and current_direction != 'up':
+            current_direction = 'down'
 
 
 def print_board():
     global win
     global black_square
     global board
+    global snake
+    global game_over
     for i in range(20):
         for j in range(20):
+            if board[i][j] == 1 and [j, i] not in snake:
+                board[i][j] = '_'
             if board[i][j] == '_':
                 win.blit(black_square, (1 + j * (26 + 1), 1 + i * (26 + 1)))
             elif board[i][j] == 1:
@@ -148,28 +174,106 @@ def print_board():
                 win.blit(head, (1 + j * (26 + 1), 1 + i * (26 + 1)))
             elif board[i][j] == 3:
                 win.blit(food, (1 + j * (26 + 1), 1 + i * (26 + 1)))
+            if game_over is True:
+                win.blit(gameover, (70, 170))
 
 
-thread1 = Thread(target=print_food, daemon=True)
-thread2 = Thread(target=print_snake, daemon=True)
+def snake_movement():
+    global current_direction
+    global event
+    global board
+    global snake
+    global game_over
+    global run
+
+    while run:
+        if current_direction == 'right':
+            x = snake[0][0]
+            y = snake[0][1]
+            board[y][x] = '_'
+            snake = snake[1:]
+            if snake[-1][0] == 19:
+                snake.append([0, snake[-1][1]])
+                snake = snake[1:]
+            if board[snake[-1][1]][snake[-1][0] + 1] == '_':
+                snake.append([snake[-1][0] + 1, snake[-1][1]])
+            elif board[snake[-1][1]][snake[-1][0] + 1] == 3:
+                snake.append([snake[-1][0] + 1, snake[-1][1]])
+                snake.append([snake[-1][0] + 1, snake[-1][1]])
+            elif board[snake[-1][1]][snake[-1][0] + 1] == 1:
+                game_over = True
+
+
+        elif current_direction == 'left':
+            x = snake[0][0]
+            y = snake[0][1]
+            board[y][x] = '_'
+            snake = snake[1:]
+            if snake[-1][0] == 0:
+                snake = snake[1:]
+                snake.append([19, snake[-1][1]])
+            if board[snake[-1][1]][snake[-1][0] - 1] == '_':
+                snake.append([snake[-1][0] - 1, snake[-1][1]])
+            elif board[snake[-1][1]][snake[-1][0] - 1] == 3:
+                snake.append([snake[-1][0] - 1, snake[-1][1]])
+                snake.append([snake[-1][0] - 1, snake[-1][1]])
+            elif board[snake[-1][1]][snake[-1][0] - 1] == 1:
+                game_over = True
+
+
+        elif current_direction == 'up':
+            x = snake[0][0]
+            y = snake[0][1]
+            board[y][x] = '_'
+            snake = snake[1:]
+            if snake[-1][1] == 0:
+                snake = snake[1:]
+                snake.append([snake[-1][0], 19])
+            if board[snake[-1][1] - 1][snake[-1][0]] == '_':
+                snake.append([snake[-1][0], snake[-1][1] - 1])
+            elif board[snake[-1][1] - 1][snake[-1][0]] == 3:
+                snake.append([snake[-1][0], snake[-1][1] - 1])
+                snake.append([snake[-1][0], snake[-1][1] - 1])
+            elif board[snake[-1][1] - 1][snake[-1][0]] == 1:
+                game_over = True
+
+
+        elif current_direction == 'down':
+            x = snake[0][0]
+            y = snake[0][1]
+            board[y][x] = '_'
+            snake = snake[1:]
+            if snake[-1][1] == 19:
+                snake = snake[1:]
+                snake.append([snake[-1][0], 0])
+            if board[snake[-1][1] + 1][snake[-1][0]] == '_':
+                snake.append([snake[-1][0], snake[-1][1] + 1])
+            elif board[snake[-1][1] + 1][snake[-1][0]] == 3:
+                snake.append([snake[-1][0], snake[-1][1] + 1])
+                snake.append([snake[-1][0], snake[-1][1] + 1])
+            elif board[snake[-1][1] + 1][snake[-1][0]] == 1:
+                game_over = True
+
+        time.sleep(0.05)
+
+
+thread1 = Thread(target=place_food, daemon=True)
+thread2 = Thread(target=snake_movement, daemon=True)
 
 thread1.start()
 thread2.start()
 print_board()
-
 
 pg.display.update()
 while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-            thread1.join()
-            thread2.join()
 
-        if event.type == pg.KEYDOWN:
+        if event.type == pg.MOUSEBUTTONDOWN or event.type == pg.KEYDOWN:
             click_buttons()
-
     print_board()
+    place_snake()
     pg.display.update()
-    pg.time.delay(20)
+    pg.time.delay(3)
 pg.quit()
